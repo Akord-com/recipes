fb802a528ca60fcd060d8d07f43f4006d9af30a2
Michal Kaliszewski <michal.kaliszewski00@gmail.com>
2023-11-06 12:24:34 +0100

    fix: polyfill the node path

--- react-cra-starter/config-overrides.js
+++ react-cra-starter/config-overrides.js
@@ -6,13 +6,13 @@ module.exports = function override(config) {
     crypto: false, // require.resolve("crypto-browserify") can be polyfilled here if needed
     stream: false, // require.resolve("stream-browserify") can be polyfilled here if needed
     fs: false, // require.resolve("fs") can be polyfilled here if needed
-    path: false, // require.resolve("assert") can be polyfilled here if needed
+    path: require.resolve("path-browserify")
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
+      Buffer: ["buffer", "Buffer"]
     }),
   ]);
   config.ignoreWarnings = [/Failed to parse source map/];
